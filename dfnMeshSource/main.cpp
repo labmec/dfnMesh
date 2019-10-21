8dc473dda8b2cc2fbe0d197fcab382698b5b408f
PedroLima92 <pedrolima.ec@outlook.com>
2019-10-21 19:41:35 -0200

    (ongoing) trying to read examples from a file

--- dfnMeshSource/main.cpp
+++ dfnMeshSource/main.cpp
@@ -34,7 +34,7 @@
 
 
 void ReadFractureFromFile(std::string filename, TPZFMatrix<REAL> &plane);
-
+void ReadExampleFromFile(std::string filename, TPZManVector<TPZFMatrix<REAL>> &planevector, TPZGeoMesh *gmesh);
 //MATERIAL ID MAP
 // 1 gmesh (default)
 // 4 skeleton
@@ -49,7 +49,13 @@ using namespace std;
 
 int main()
 {
+	Matrix plane0(3,4);
+	Matrix plane1(3,4);
+	TPZManVector<TPZFMatrix<REAL>> planevector(2);
+	planevector[0] = plane0;
+	planevector[1] = plane1;
 
+	ReadExampleFromFile("example.txt",planevector,nullptr);
 	
 
 	// Creating the Geo mesh
@@ -209,4 +215,238 @@ void ReadFractureFromFile(std::string filename, TPZFMatrix<REAL> &plane){
 		}
 		std::cout << std::endl;
 	}
+}
+
+
+void ReadExampleFromFile(std::string filename, TPZManVector<TPZFMatrix<REAL>> &planevector, TPZGeoMesh *gmesh){
+	/*_______________________________________________________________
+						FILE FORMAT 
+
+		Domain 								//dimensions of the domain
+		Lx Ly Lz (double)
+
+		Mesh			
+		2D ELTYPE (string)
+		Nx Ny Nz (int)						// number of divisions
+
+		NumberOfFractures [N] (int)
+
+		Fracture 0	[ncorners]				// coordinates for j corner nodes
+		[x0] [x1] ... [xj]
+		[y0] [y1] ... [yj]
+		[z0] [z1] ... [zj]
+
+		Fracture 1	[ncorners]
+		...
+		Fracture N	[ncorners]
+		_______________________________________________________________
+						EXAMPLE
+		Domain
+		2.0 2.0 2.0
+
+		Mesh
+		EQuadrilateral
+		2 2 2
+
+		NumberOfFractures 2
+
+		Fracture 0 4
+		0.3 1.3 1.3 0.3
+		0.3 0.3 1.4 1.4
+		0.7 0.7 0.5 0.5
+		
+		Fracture 1 4
+		0.6 1.6 1.6 0.6
+		0.55 0.7 0.55 0.40
+		1.3 1.3 0.25 0.25
+	 */
+
+	string line, word;
+	const string Domain = "Domain";
+	const string Mesh("Mesh"), Fractures("NumberOfFractures");
+	int i, j, nfractures;
+	REAL Lx, Ly, Lz;
+	MElementType eltype;
+	int nx, ny, nz;
+
+	// Read file
+	ifstream plane_file(filename);
+	if (!plane_file){
+		std::cout << "Error reading file" << std::endl;
+		DebugStop();
+	}
+	// Go through it line by line
+	while (getline(plane_file, line)){
+		{
+			std::stringstream ss(line);
+			getline(ss, word, ' ');
+		}
+		if(word == "Domain"){
+			getline(plane_file, line);
+			std::stringstream ss(line);
+			getline(ss, word, ' ');
+			while (word.length() == 0){getline(ss, word, ' ');}
+			Lx = std::stod(word);
+			getline(ss, word, ' ');
+			while (word.length() == 0){getline(ss, word, ' ');}
+			Ly = std::stod(word);
+			getline(ss, word, ' ');
+			while (word.length() == 0){getline(ss, word, ' ');}
+			Lz = std::stod(word);
+		}
+
+		{
+			getline(plane_file, line);
+			while(line.length() == 0){getline(plane_file, line);}
+			std::stringstream ss(line);
+			getline(ss, word, ' ');
+		}
+		if(word == "Mesh"){
+			getline(plane_file, line);
+			{
+				std::stringstream ss(line);
+				getline(ss, word, ' ');
+				while (word.length() == 0){getline(ss, word, ' ');}
+				if(word == "EQuadrilateral"){
+					eltype = EQuadrilateral;
+				}else if(word == "ETriangle"){
+					eltype = ETriangle;
+				}else{ std::cout<<"\nError reading file\n"; DebugStop();}
+			}
+			getline(plane_file, line);
+			{
+				std::stringstream ss(line);
+				getline(ss, word, ' ');
+				while (word.length() == 0){getline(ss, word, ' ');}
+				nx = std::stoi(word);
+				getline(ss, word, ' ');
+				while (word.length() == 0){getline(ss, word, ' ');}
+				ny = std::stoi(word);
+				getline(ss, word, ' ');
+				while (word.length() == 0){getline(ss, word, ' ');}
+				nz = std::stoi(word);
+			}
+		}
+
+		{
+			getline(plane_file, line);
+			while(line.length() == 0){getline(plane_file, line);}
+			std::stringstream ss(line);
+			getline(ss, word, ' ');
+			if(word == "NumberOfFratures") {
+				getline(ss, word, ' ');
+				while (word.length() == 0){getline(ss, word, ' ');}
+				nfractures = std::stoi(word);
+			}
+		}
+		planevector.resize(nfractures);
+		for(int ifrac = 0; ifrac < nfractures; ifrac++){
+			string aux;
+			while(aux != "Fracture"){
+				getline(plane_file, line);
+				std::stringstream ss(line);
+				getline(ss, aux, ' ');
+				while (aux.length() == 0){getline(ss, aux, ' ');}
+			}
+			{
+				std::stringstream ss(line);
+				getline(ss, word, ' ');
+				while (word.length() == 0 || word == "Fracture"){getline(ss, word, ' ');}
+			}
+			int fracid = std::stoi(word);
+			{
+				std::stringstream ss(line);
+				getline(ss, word, ' ');
+				while (word.length() == 0 || word == "Fracture"){getline(ss, word, ' ');}
+			}
+			int ncorners = std::stoi(word);
+			planevector[ifrac].Resize(3,ncorners);
+			std::cout<<"\nCorners of fracture #"<<fracid<<":\n";
+			for(int i=0; i<3; i++){
+				getline(plane_file, line);
+				std::stringstream ss(line);
+				int j = 0;
+				while (getline(ss, word, ' ')){
+					while (word.length() == 0){getline(ss, word, ' ');}
+					planevector[ifrac](i, j) = std::stod(word);
+					std::cout << planevector[ifrac](i, j) << (j<ncorners-1?", \t":"\n");
+					j++;
+				}
+			}
+			std::cout<<"\n";
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// Creating the Geo mesh
+
+	int dimel = 2;
+	TPZManVector<REAL, 3> x0(3, 0.), x1(3, 2);
+	x1[2] = 0.;
+	TPZManVector<int, 2> nelx(2, dimel);
+	TPZGenGrid gengrid(nelx, x0, x1);
+	gengrid.SetElementType(EQuadrilateral);
+	TPZGeoMesh *gmesh = new TPZGeoMesh;
+	gmesh->SetDimension(2);
+	gengrid.Read(gmesh);
+
+	// Mesh 3D
+
+	TPZExtendGridDimension extend(gmesh,1.);
+	extend.SetElType(1);
+	TPZGeoMesh *gmesh3d = extend.ExtendedMesh(dimel);
+	gmesh = gmesh3d;
+
+	// count number of corners
+	while (npoints == 0){
+		ifstream plane_file(filename);
+		if (!plane_file){
+			std::cout << "Error reading file" << std::endl;
+			DebugStop();
+		}
+		getline(plane_file, line);
+		std::stringstream ss(line);
+		while (getline(ss, value, ' ')){
+			while (value.length() == 0){
+				getline(ss, value, ' ');
+			}
+			npoints++;
+		}
+	}
+	// then read points
+	plane.Resize(3,npoints);
+	{ //just a scope
+		int i = 0;
+		int j = 0;
+		std::cout << "Fracture plane defined as: \n";
+		ifstream plane_file(filename);
+		while (getline(plane_file, line)){
+			std::stringstream ss(line);
+			while (getline(ss, value, ' ')){
+				while (value.length() == 0){
+					getline(ss, value, ' ');
+				}
+				plane(i, j) = std::stod(value);
+				std::cout << plane(i, j) << (j<npoints-1?" , ":"\n");
+				j++;
+			}
+			j = 0;
+			i++;
+		}
+		std::cout << std::endl;
+	}
+*/
 }
