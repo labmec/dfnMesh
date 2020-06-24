878d78cb1b1c33ac79d9bea259967474cfa45519
PedroLima92 <pedrolima.ec@outlook.com>
2020-06-23 21:06:17 -0300

    animations

--- drafts/draft.py
+++ drafts/draft.py
@@ -1,8 +1,71 @@
-input = self.GetUnstructuredGridInput() 
-output = self.GetUnstructuredGridOutput()
-output.GetPointData().PassData(input.GetPointData())
-output.GetCellData().PassData(input.GetCellData()) 
-cellTypes = vtk.vtkUnsignedCharArray() 
-cellTypes.DeepCopy(input.GetCellTypesArray())
-cellTypes.SetName("Cell Types") 
-output.GetCellData().AddArray(cellTypes)
+
+# vtkmesh
+# ElementRange
+#     3D
+#         CoarseMesh
+#     1D
+#         iPoints
+#         Outline
+#         Ribs intact
+#         Ribs cut
+#     shrink
+#         2D
+#             intactFaces
+#             Refinements
+#             SurfaceMesh
+#         3D
+#             submesh
+#     2D
+#         Polygons
+
+
+
+
+
+# trace generated using paraview version 5.8.0
+#
+# To ensure correct image size when batch processing, please search 
+# for and uncomment the line `# renderView*.ViewSize = [*,*]`
+
+#### import the simple module from the paraview
+from paraview.simple import *
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# find source
+vtkmesh = FindSource('vtkmesh.*')
+
+# create a new 'Threshold'
+threshold1 = Threshold(Input=vtkmesh)
+threshold1.Scalars = ['CELLS', 'elIndex']
+threshold1.ThresholdRange = [0.0, 10000.0]
+RenameSource('ElementRange', threshold1)
+
+# create a new 'Threshold'
+threshold1_1 = Threshold(Input=threshold1)
+threshold1_1.Scalars = ['CELLS', 'Dimension']
+threshold1_1.ThresholdRange = [3.0, 3.0]
+RenameSource('3D', threshold1_1)
+# create a new 'Threshold'
+threshold1_1_1 = Threshold(Input=threshold1_1)
+threshold1_1_1.Scalars = ['CELLS', 'material']
+threshold1_1_1.ThresholdRange = [1.0, 1.0]
+RenameSource('CoarseMesh', threshold1_1_1)
+
+
+# create a new 'Threshold'
+threshold1_2 = Threshold(Input=threshold1)
+threshold1_2.Scalars = ['CELLS', 'Dimension']
+threshold1_2.ThresholdRange = [1.0, 1.0]
+RenameSource('1D', threshold1_2)
+# create a new 'Threshold'
+threshold1_2_1 = Threshold(Input=threshold1_2)
+threshold1_2_1.Scalars = ['CELLS', 'material']
+threshold1_2_1.ThresholdRange = [2.0, 2.0]
+RenameSource('iPoints', threshold1_2_1)
+
+
+
+#### uncomment the following to render all views
+RenderAllViews()
+# alternatively, if you want to write images, you can use SaveScreenshot(...).
---
+++ scripts/rotating-fracture.py
@@ -0,0 +1,67 @@
+# @brief Runs DFNMesh with rotating fracture in 2D and generate multiple vtk files to get a paraview animation
+# @author github/PedroLima92
+# @date june 2020
+
+import subprocess
+import math
+
+# ../dfnMesh/<vtkfile.vtk>
+vtkfile = "vtkmesh"
+# ../examples/<example>
+example = "scripts/rotating-fracture.txt"
+# ../examples/<msh>
+msh = "no-msh-file"
+
+preamble = (
+"""Domain
+8.0 8.0 0.0
+
+Mesh
+EQuadrilateral
+4 4 0
+
+NumberOfFractures 1
+
+Fracture 0 4"""
+)
+z = "\n-1.00  1.00  1.00 -1.00"
+
+toldist = 0.5
+step = 0.5
+# steps = 0
+steps = int(180/step)
+step = step*math.pi/180
+steps += 1
+x0 = 4.0
+y0 = 4.0
+r = 5.66
+theta = 0
+
+for i in range(steps):
+    f = open(example,"w+")
+    f.write(preamble)
+    x1 = x0+r*math.cos(theta)
+    y1 = y0+r*math.sin(theta)
+    x2 = x0-r*math.cos(theta)
+    y2 = y0-r*math.sin(theta)
+    x = ("\n %.2f %.2f %.2f %.2f" % (x1,x1,x2,x2))
+    y = ("\n %.2f %.2f %.2f %.2f" % (y1,y1,y2,y2))
+    f.write(x)
+    f.write(y)
+    f.write(z)
+    f.close()
+    # run dfnMesh
+    print(toldist)
+    dfnMesh = ["build/src/dfnTest"
+              ,example
+              ,msh
+              ,str(toldist)]
+    subprocess.call(dfnMesh)
+    rename = ["cp"
+              , vtkfile+".vtk"
+              , vtkfile+"."+str(i)+".vtk"]
+    subprocess.call(rename)
+    theta += step
+
+
+# for i in range(steps):
