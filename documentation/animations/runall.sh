64211a84a36cc13f9cbd71177ca7cf39fb55bcc4
Pedro Lima <pedrolima.ec@outlook.com>
2020-10-25 20:18:26 -0300

    A script to run all animations at once

---
+++ documentation/animations/runall.sh
@@ -0,0 +1,15 @@
+
+
+cd ~/projetos/dfnMesh
+
+python3 documentation/animations/anim1/variable-tol.py
+python3 documentation/animations/anim2/rotating-fracture.py
+python3 documentation/animations/anim3/sweep-cubes.py
+python3 documentation/animations/anim4/non-convex-polyhedra.py
+python3 documentation/animations/anim5/tetrahedra1.py
+python3 documentation/animations/beijing2/beijing2.py
+python3 documentation/animations/cilamce/ex1/cilamce1.py
+python3 documentation/animations/cilamce/ex2/cilamce2.py
+
+
+cd ~/projetos/dfnMesh
