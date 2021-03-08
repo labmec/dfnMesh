labmec/dfnMesh

commit 471a95227d940d9d66e5463efdc6e46cb89aed7f

Included written suggestions but also a major execution time optimization

src/DFNFracture.cpp
@@ -376,6 +376,7 @@ void DFNFracture::SnapIntersections_faces(REAL tolDist, REAL tolAngle){
         LOGPZ_INFO(logger,stream.str());
     }
 #endif // LOG4CXX
+    // @pedro - what is the purpose of this method?
     SearchForSpecialQuadrilaterals();
 }
 
@@ -383,7 +384,8 @@ void DFNFracture::SnapIntersections_faces(REAL tolDist, REAL tolAngle){
 
 
 bool DFNFracture::IsSpecialQuadrilateral(TPZGeoEl* gel){
-    if(gel->Type() != MElementType::EQuadrilateral) return false;
+    // @pedro - some methods should be called for a purpose
+    if(gel->Type() != MElementType::EQuadrilateral) DebugStop();
 
     // If this DFNFracture has already found me, and has decided I'm not special, you can move on to the next quadrilateral
     DFNFace* dfnface = Face(gel->Index());
@@ -902,7 +904,9 @@ void DFNFracture::MeshFractureSurface(){
             // Check if would-be polygon already exists in the mesh before trying to mesh it
             TPZGeoEl* ExistingGel = FindPolygon(subpolygon);
             if(ExistingGel){
-                InsertFaceInSurface(ExistingGel->Index());
+                TPZGeoElSide gelside(ExistingGel);
+                TPZGeoElBC gbc(gelside,this->fIndex+10);
+                InsertFaceInSurface(gbc.CreatedElement()->Index());
                 continue;
             }
             MeshPolygon(subpolygon);
@@ -1066,7 +1070,9 @@ void DFNFracture::MeshPolygon_GMSH(TPZStack<int64_t>& orientedpolygon, std::set<
         gmsh::model::geo::addPlaneSurface(wiretag,wiretag[0]);
     }
     // gmsh::model::addPhysicalGroup(2,wiretag,this->fmatid);
-    gmsh::model::addPhysicalGroup(2,wiretag,DFNMaterial::Eintact);
+    // @pedro mudei aqui
+//    gmsh::model::addPhysicalGroup(2,wiretag,DFNMaterial::Eintact);
+    gmsh::model::addPhysicalGroup(2,wiretag,this->fIndex+10);
 
 	
 	// synchronize before meshing
@@ -1105,6 +1111,9 @@ void DFNFracture::MeshPolygon(TPZStack<int64_t>& polygon){
 	}
     
     SetLoopOrientation(polygon);
+    nedges = polygon.size();
+//    std::cout << "Polygon coords\n";
+//    for(auto no : nodes) gmesh->NodeVec()[no].Print();
     // std::cout<<"SubPolygon# "<<polygon_counter<<": "<<polygon<<std::endl;
     // If polygon is planar quadrilateral or triangle, we can skip gmsh
     bool isplane = DFN::AreCoPlanar(gmesh,nodes);
@@ -1115,7 +1124,7 @@ void DFNFracture::MeshPolygon(TPZStack<int64_t>& polygon){
         case 3:             
         case 4: if(isplane){
                     // TPZGeoEl* newel = DFN::MeshSimplePolygon(gmesh,polygon,this->fmatid); 
-                    TPZGeoEl* newel = DFN::MeshSimplePolygon(gmesh,polygon,DFNMaterial::Eintact); 
+                    TPZGeoEl* newel = DFN::MeshSimplePolygon(gmesh,polygon,fIndex+10);
                     newelements[0] = newel->Index();
                     break;
                 }

src/DFNFace.cpp
@@ -827,6 +827,7 @@ bool DFNFace::SnapIntersection_try(REAL tolDist, REAL tolAngle_cos){
         // @pedro : how are you sure that "all" sides need to be snapped?
         // can it not be that a single rib needs to be snapped?
 		// @reply: I agree, this was a mistake of mine, I'll rewrite the method.
+        // thanks!!
 		this->SnapIntersection_force();
         return true;
     }

src/DFNFracture.h
@@ -111,8 +111,27 @@ public:
 
     int NSurfElements() const{return fSurfaceFaces.size();}
     
+    // return the indices of all polyhedra intersected by the fracture
+    // @pedro - please implement
+    void IdentifyIntersectedPolyhedra(std::set<int64_t> &polyset);
+    
+    // return the indices of the geometric elements that belong to the discretized fracture
+    // through snapping AND were in the original mesh
+    // @pedro please implement - based on the ribs and their snap status
+    void IdentifySnapRibs(std::set<int64_t> &gelindices);
+    
+    // facet set of 2d elements with the same normal direction
+    // divide facets of polyhedra that have non-colinear snap-ribs
+    // loop over polyhedra
+    // identify the snap-ribs the belong to the polyhedra
+    // verify if the snap-ribs require meshing of a facet (hard)
+    // divide the facet (hard part)
+    // @pedro - please implement me
+    void MeshSnapPlanes();
+    
 private:
 
+    
     void Initialize(DFNPolygon &Polygon, DFNMesh *dfnMesh, FracLimit limithandling = Eextended);
         
     /// Checks neighbour's dimension and returns true if it is equal

src/DFNMesh.cpp
@@ -1820,7 +1820,8 @@ void DFNMesh::UpdatePolyhedra(){
 			bool IsConvex = true;
 			int coarseindex = -1;
 
-			std::cout<< ' ' << loading[(buffering++%4)] << '\r' << std::flush;
+            // @pedro commented this line - didnt understand this output
+//			std::cout<< ' ' << loading[(buffering++%4)] << '\r' << std::flush;
 				
 			BuildVolume(initial_face_orient,IsConvex,polyhedron,coarseindex);
 

src/DFNMesh.h
@@ -34,6 +34,8 @@ private:
     TPZGeoMesh *fGMesh = nullptr;
 
     /// For each edge, a vector of sorted faces around that edge
+    // @pedro this is a necessary data structure for building the polyhedra
+    // but does it need to be permanent?
     TPZVec<TRolodex> fSortedFaces;
 
     /** 

src/DFNPolyhedron.h
@@ -17,8 +17,10 @@ class DFNMesh;
 // class DFNFracture;
 
 /*! 
- *  @brief      Describes a convex polyhedral volume as a set of oriented faces that enclose the shell around it. (not to be confused with DFNPolygon)
+ *  @brief Describes a convex polyhedral volume as a set of oriented faces that enclose the shell around it. (not to be confused with DFNPolygon)
  */
+// @pedro - what is the difference/relation between a DFNPolyhedron and a DFNVolume?
+// looked at DFNVolume that is deprecated...
 class DFNPolyhedron
 {
     private:

src/DFNVolume.h
@@ -34,6 +34,8 @@ class DFNVolume
     /// Index of in-volume intersection EPoint
     int64_t fIntersection = -1;
     
+    // @pedro there is no such thing as a "plane" in the datastructure, do you mean 2d geometric
+    // elements that form the volume boundary
     /// Indices of planes enclosed by volume
     TPZManVector<int64_t> fEnclosedFaces;
 

src/main.cpp
@@ -186,7 +186,9 @@ int main(int argc, char* argv[]){
         // snap the intersection point to the nearest node
 		fracture->SnapIntersections_ribs(tol_dist);
 		// create DFNFace objects - these are faces that have intersected ribs (always 2)
+        // @pedro - please change the name to create face objects
 		fracture->FindFaces();
+        // this method will snap the ribs with small angles to coincide with
 		fracture->SnapIntersections_faces(tol_dist,tol_angle);
 
 #ifdef LOG4CXX
@@ -196,18 +198,43 @@ int main(int argc, char* argv[]){
             LOGPZ_DEBUG(logger,sout.str());
         }
 #endif
+        // we decided that the ribs can be cut. Apply the refinement to the geometric elements
 		fracture->RefineRibs();
+        // apply the refinement to the faces
+        // @pedro - shouldnt we verify if the snap created a fracture line across a face
+        // maybe we need the notion of snap-ribs - ribs that belong to the fracture but
+        // are the result of a snap operation
+        // set of snap-ribs is suggested data structure of DFNFracture
+        // problems occurr when snap-ribs force the division of a face that was not intersected
+        // by the fracture plane "in the first place"
+        // In order to "know" if a fracture line will cross a face, we need to know the
+        // for each set of coplanar faces belonging to a same polyhedra a set of neighbouring snap-ribs
+        
+        // For each fracture, we need to identify the set of polyhedra - 
+        // For each intersected polyhedra, we need to verify if the snap-ribs will induce a refinement
+        // of one of its facets
+        // this method must be implemented in DFNFracture as it affects objects out of the scope
+        // of DFNFace and DFNRib
+        
+        
 		fracture->RefineFaces();
 	// Mesh fracture surface
 		if(gmesh->Dimension() == 3){
 			// dfn.InheritPolyhedra();
+            // divide the fracture in simple geometries using the mesh created in RefineFaces
 			fracture->MeshFractureSurface();
 			// dfn.DumpVTK();
+            // this is where the code can crash : a face that should be internal can be aligned
+            // with an existing face
 			dfn.UpdatePolyhedra();
 		}
 #ifdef PZDEBUG
-		std::ofstream logtest("LOG/dfn.summary.txt");
-		dfn.Print(logtest,argv[1]);
+        {
+            std::ofstream logtest("LOG/dfn.summary.txt");
+            dfn.Print(logtest,argv[1]);
+            std::ofstream out("gmesh.vtk");
+            TPZVTKGeoMesh::PrintGMeshVTK(gmesh, out);
+        }
 #endif //PZDEBUG
 	}
 	// Recover Limits

commit 3b775ae64ef7742031b7c13c25c98c5408b8a4a6

BugFix (bug introduced in commit 471a952)

src/DFNFracture.cpp
@@ -904,9 +904,10 @@ void DFNFracture::MeshFractureSurface(){
             // Check if would-be polygon already exists in the mesh before trying to mesh it
             TPZGeoEl* ExistingGel = FindPolygon(subpolygon);
             if(ExistingGel){
-                TPZGeoElSide gelside(ExistingGel);
-                TPZGeoElBC gbc(gelside,this->fIndex+10);
-                InsertFaceInSurface(gbc.CreatedElement()->Index());
+                // TPZGeoElSide gelside(ExistingGel);
+                // TPZGeoElBC gbc(gelside,this->fIndex+10);
+                // InsertFaceInSurface(gbc.CreatedElement()->Index());
+                InsertFaceInSurface(ExistingGel->Index());
                 continue;
             }
             MeshPolygon(subpolygon);